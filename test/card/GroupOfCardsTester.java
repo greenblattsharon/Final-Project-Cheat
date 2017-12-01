669a91fe903b8fded4eb15697c1889a35b09f7c8
sharoniegreenblatt <sharonie21@gmail.com>
2017-11-30 21:11:44 -0500

    Updated test cases for GroupOfCards

--- test/card/GroupOfCardsTester.java
+++ test/card/GroupOfCardsTester.java
@@ -1,6 +1,5 @@
 package card;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -33,4 +32,50 @@ public class GroupOfCardsTester {
         goc.removeCard(new Card(5, Suit.Diamond));
         assertTrue("Should display one card in size", 0 == goc.getSize());
     }
+
+    /**
+     * Verify cannot add same cards
+     */
+    @Test(expected = IllegalCardException.class)
+    public void shouldRejectAddingSameCards() throws IllegalCardException{
+        goc.addCard(new Card(5, Suit.Diamond));
+        goc.addCard(new Card(5, Suit.Diamond));
+    }
+
+    /**
+     * Verify cannot remove cards from empty deck
+     */
+    @Test(expected = IllegalCardException.class)
+    public void shouldRejectRemovingFromEmpty() throws IllegalCardException{
+        goc.removeCard(new Card(5, Suit.Diamond));
+
+    }
+
+    /**
+     * Returns correct size of deck
+     */
+    @Test
+    public void shouldReturnCorrectDeckSize() throws IllegalCardException{
+        goc.addCard(new Card(5, Suit.Diamond));
+        goc.addCard(new Card(6, Suit.Diamond));
+        assertTrue("There should be two cards in the deck", 2 == goc.getSize());
+    }
+
+    /**
+     * Verify that empty deck returns true
+     */
+    @Test
+    public void shouldReturnTrueForEmpty(){
+        assertTrue("Method returns true for empty", goc.isItEmpty());
+    }
+
+    /**
+     * Verify that not empty deck returns false
+     */
+    @Test
+    public void shouldNotReturnTrueForEmpty() throws IllegalCardException{
+        goc.addCard(new Card(5, Suit.Diamond));
+        assertFalse("Method returns false for empty", goc.isItEmpty());
+    }
+
 }
