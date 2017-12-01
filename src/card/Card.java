17e9d05b56c7682a37a2eb427dc623d81527a5dd
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-01 14:05:35 -0500

    Added final static variables for Ace, Jack, Queen, King

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,6 +1,12 @@
 package card;
 
 public class Card {
+
+    public final static int ACE = 1,          // Codes for the non-numeric cards.
+                            JACK = 11,        //   Cards 2 through 10 have their
+                            QUEEN = 12,       //   numerical values for their codes.
+                            KING = 13;
+
     private int number;
     private Suit suit;
 
