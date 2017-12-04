196c09ea0f3c63999ab526a756adbfcbbccd2aed
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-04 16:06:45 -0500

    Updated Deck to include dealing cards

--- src/card/Deck.java
+++ src/card/Deck.java
@@ -1,6 +1,12 @@
 package card;
 
+import player.Player;
+
+import java.util.ArrayList;
+
 public class Deck extends GroupOfCards {
+
+    private final static int SIZE_OF_DECK = 52;
 
     public void initializeDeck() throws IllegalCardException {
         for(Suit suit: Suit.values()){
@@ -8,6 +14,23 @@ public class Deck extends GroupOfCards {
                 addCard(new Card(i, suit));
             }
         }
+    }
 
+    public void dealCards(Player[] players) throws IllegalCardException {
+        shuffleCards();
+        int counter = 0;
+        Card card;
+        ArrayList<Card> cards = getCards();
+
+        while(getSize() != 0){
+            card = cards.get(0);
+            players[counter].getHand().addCard(card);
+            removeCard(card);
+            counter++;
+
+            if (counter >= players.length){
+                counter = 0;
+            }
+        }
     }
 }
