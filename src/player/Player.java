8826e4160463faf64b0a22387cdedf82cdd11853
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-04 17:15:52 -0500

    Created Player abstract class

---
+++ src/player/Player.java
@@ -0,0 +1,22 @@
+package player;
+
+import card.Card;
+import card.Hand;
+
+public abstract class Player {
+
+    private Hand hand = new Hand();
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public boolean callCheat(){
+        return true;
+    }
+
+    public Card[] playCards(int[] index){
+
+        return null;
+    }
+}
