ca5890b06a0d3e62ec7c7e10bfb12b35b34c4ef8
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-04 16:05:47 -0500

    Updated DeckTester to test dealing cards

--- test/card/DeckTester.java
+++ test/card/DeckTester.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import player.Player;
+import player.User;
 
 public class DeckTester {
     Deck deck;
@@ -17,8 +19,54 @@ public class DeckTester {
     @Test
     public void newDeckWillHave52Cards() throws IllegalCardException {
         deck.initializeDeck();
-        System.out.println(deck.getSize());
         assertTrue("New deck will have 52 cards", 52 == deck.getSize());
     }
+
+    /**
+     * Dealing cards to one player will result in the one player having a hand of 52 cards
+     */
+    @Test
+    public void dealToOnePlayer() throws IllegalCardException{
+        deck.initializeDeck();
+        Player player = new User();
+        Player[] players = new Player[1];
+
+        players[0] = player;
+
+        deck.dealCards(players);
+
+        assertTrue("Player will have hand of 52 cards", 52 == player.getHand().getSize());
+
+    }
+
+    /**
+     * Dealing cards to four players will result in the each player having a hand of 13 cards
+     */
+    @Test
+    public void dealToFourPlayers() throws IllegalCardException{
+        deck.initializeDeck();
+
+        Player player1 = new User();
+        Player player2 = new User();
+        Player player3 = new User();
+        Player player4 = new User();
+        
+        Player[] players = new Player[4];
+
+        players[0] = player1;
+        players[1] = player2;
+        players[2] = player3;
+        players[3] = player4;
+
+        deck.dealCards(players);
+
+        assertTrue("Player 1 will have hand of 13 cards", 13 == players[0].getHand().getSize());
+        assertTrue("Player 2 will have hand of 13 cards", 13 == players[1].getHand().getSize());
+        assertTrue("Player 3 will have hand of 13 cards", 13 == players[2].getHand().getSize());
+        assertTrue("Player 4 will have hand of 13 cards", 13 == players[3].getHand().getSize());
+
+    }
+
+
 
 }
