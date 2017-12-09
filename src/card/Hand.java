2d695d606be73696075a7c4838f634b0d60078f1
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-08 22:05:19 -0500

    Added printCards to help with testing

--- src/card/Hand.java
+++ src/card/Hand.java
@@ -1,4 +1,13 @@
 package card;
 
+import java.util.ArrayList;
+
 public class Hand extends GroupOfCards {
+
+    public void printCards(){
+        ArrayList<Card> cards = getCards();
+        for(Card card: cards){
+            System.out.println("Number: " + card.getNumber() + "Suit: " + card.getSuit());
+        }
+    }
 }
