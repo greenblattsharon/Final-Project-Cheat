c3cffd81f338fd4e4e67ef62fd796b6d572e63d0
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-09 18:32:10 -0500

    Added getCardsIndicies

--- src/player/Player.java
+++ src/player/Player.java
@@ -3,6 +3,8 @@ package player;
 import card.Card;
 import card.Hand;
 
+import java.util.ArrayList;
+
 public abstract class Player {
 
     private Hand hand = new Hand();
@@ -20,6 +22,20 @@ public abstract class Player {
         return null;
     }
 
-    public abstract int[] getCardsToPlay();
+    public abstract int[] getCardsToPlay(int card_number);
+
+    public int[] getCardIndicies(int card_number, int count){
+        int[] cards_indicies = new int[count];
+        int counter = 0;
+        ArrayList<Card> cards = hand.getCards();
+
+        for (int i = 0; i < hand.getSize(); i++) {
+            if (cards.get(i).getNumber() == card_number) {
+                cards_indicies[counter] = i;
+                counter++;
+            }
+        }
+        return cards_indicies;
+    }
 
 }
