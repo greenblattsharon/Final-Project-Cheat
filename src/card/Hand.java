4b3c0b21098397dbd2622dcd634852e612bd0f51
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-08 22:14:09 -0500

    Updated Hand

--- src/card/Hand.java
+++ src/card/Hand.java
@@ -7,7 +7,7 @@ public class Hand extends GroupOfCards {
     public void printCards(){
         ArrayList<Card> cards = getCards();
         for(Card card: cards){
-            System.out.println("Number: " + card.getNumber() + "Suit: " + card.getSuit());
+            System.out.println("Number: " + card.getNumber() + " Suit: " + card.getSuit());
         }
     }
 }
