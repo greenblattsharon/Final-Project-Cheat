51537df41afe60c854e1301139d33f6435c9bfa9
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-09 23:16:49 -0500

    Updated to initialize parts of the game

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,20 +2,35 @@ package game;
 
 import card.Card;
 import card.Deck;
+import card.IllegalCardException;
 import player.Player;
 
 public class Game {
-    private Player turn;
-    private boolean cheat = false;
-    private boolean lie = false;
+    public Player turn;
+    public boolean cheat = false;
+    public boolean lie = false;
 
-    private Card last_card = null;
+    public Card last_card = null;
 
-    private Deck deck = new Deck();
-    private Player[] players = new Player[4];
+    public int level;
 
-    public void initializeGame(int level){
+    public Deck deck;
+    public Player[] players = new Player[4];
 
+    private GameState currentState;
+
+    public Game(int level) throws IllegalCardException{
+        this.level = level;
+        currentState = new InitializeState(this);
     }
+
+    public GameState getCurrentState(){
+        return this.currentState;
+    }
+
+    public void setCurrentState(GameState gameState){
+        this.currentState = gameState;
+    }
+
 
 }
