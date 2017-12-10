70d6e091cbaab34dcc986712bf22b866a6e10e33
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-09 23:16:21 -0500

    Updated to initialize parts of the game

--- src/game/InitializeState.java
+++ src/game/InitializeState.java
@@ -1,4 +1,34 @@
 package game;
 
+import card.Deck;
+import card.DeckFactory;
+import card.IllegalCardException;
+import player.Opponent;
+import player.OpponentFactory;
+import player.User;
+import player.UserFactory;
+
 public class InitializeState implements GameState{
+
+    Game game = null;
+    GUIBuilder guiBuilder = new GUIBuilder();
+
+    public InitializeState(Game game) throws IllegalCardException{
+        this.game = game;
+        initializeGame();
+    }
+
+    private void initializeGame() throws IllegalCardException{
+        game.deck = (Deck) guiBuilder.buildGroupOfCards(new DeckFactory());
+        game.deck.initializeDeck();
+
+        game.players[0] = (User) guiBuilder.buildPlayer(new UserFactory(), 0.0);
+        game.players[1] = (Opponent) guiBuilder.buildPlayer(new OpponentFactory(), game.level * .1);
+        game.players[2] = (Opponent) guiBuilder.buildPlayer(new OpponentFactory(), (game.level * .1) + .1);
+        game.players[3] = (Opponent) guiBuilder.buildPlayer(new OpponentFactory(), (game.level * .1) + .2);
+
+        game.deck.dealCards(game.players);
+
+    }
+
 }
