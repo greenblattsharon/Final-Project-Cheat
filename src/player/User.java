d733c84368abbd9ed975d8ffd51421eeac71c9f5
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-09 19:12:59 -0500

    Added sort

--- src/player/User.java
+++ src/player/User.java
@@ -1,4 +1,16 @@
 package player;
 
+import java.util.Collections;
+
 public class User extends Player {
+
+
+    @Override
+    public int[] getCardsToPlay(int card_number) {
+        return new int[0];
+    }
+
+    public void sort(){
+        Collections.sort(getHand().getCards());
+    }
 }
