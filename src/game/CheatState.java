a226605fcefd37191db5e2698f3241f9aad812f7
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-10 20:50:56 -0500

    Updated implementStateResponsibilities

--- src/game/CheatState.java
+++ src/game/CheatState.java
@@ -1,6 +1,11 @@
 package game;
 
+import card.Card;
+import card.Hand;
 import card.IllegalCardException;
+import player.IllegalMoveException;
+
+import java.util.ArrayList;
 
 public class CheatState implements GameState{
     Game game;
@@ -10,7 +15,34 @@ public class CheatState implements GameState{
     }
 
     @Override
-    public void implementStateResponsibilities() throws IllegalCardException {
+    public void implementStateResponsibilities() throws IllegalCardException, IllegalMoveException {
+        Hand hand;
+        ArrayList<Card> cards = game.deck.getCards();
+
+        if(game.cheat == -1){
+            throw new IllegalMoveException("Cheat was improperly called");
+        }
+
+        if(game.lie){
+           hand = game.players[game.turn].getHand();
+
+           for(Card card: cards){
+               hand.addCard(card);
+               game.deck.removeCard(card);
+           }
+
+        }
+
+        else{
+            hand = game.players[game.cheat].getHand();
+
+            for(Card card: cards){
+                hand.addCard(card);
+                game.deck.removeCard(card);
+            }
+        }
+
+        game.setCurrentState(new TurnState(game));
 
     }
 }
