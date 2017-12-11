48f20b147bf87a49406b7f916047a0b86618d718
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-10 20:40:42 -0500

    Initialize CheatState

--- src/game/CheatState.java
+++ src/game/CheatState.java
@@ -3,6 +3,12 @@ package game;
 import card.IllegalCardException;
 
 public class CheatState implements GameState{
+    Game game;
+
+    public CheatState(Game game){
+        this.game = game;
+    }
+
     @Override
     public void implementStateResponsibilities() throws IllegalCardException {
 
