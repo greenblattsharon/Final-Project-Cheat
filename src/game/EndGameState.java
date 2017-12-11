f82ff8af4c27f766b3406469ee73b25f2d94126c
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-10 20:42:05 -0500

    Initialized EndGameState constructor

--- src/game/EndGameState.java
+++ src/game/EndGameState.java
@@ -4,6 +4,11 @@ import card.IllegalCardException;
 
 public class EndGameState implements GameState {
 
+    Game game;
+
+    public EndGameState(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void implementStateResponsibilities() throws IllegalCardException {
