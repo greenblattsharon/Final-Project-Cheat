707464126a3c018dd1eeaeb81a5f38f9adcf75ee
sharoniegreenblatt <sharonie21@gmail.com>
2017-12-10 21:36:11 -0500

    Added test for wrong cheat

--- test/game/CheatStateTester.java
+++ test/game/CheatStateTester.java
@@ -3,21 +3,32 @@ package game;
 import card.IllegalCardException;
 import org.junit.Before;
 import org.junit.Test;
+import player.IllegalMoveException;
 
 public class CheatStateTester {
 
     Game game;
 
+
     @Before
     public void setup() throws IllegalCardException {
         game = new Game(1);
+        game.setCurrentState(new CheatState(game));
+    }
+
+    /**
+     * If cheat is -1 then IllegalMoveException is thrown
+     */
+    @Test(expected = IllegalMoveException.class)
+    public void ifIncorrectCheatThenExceptionIsThrown() throws IllegalMoveException, IllegalCardException {
+        game.getCurrentState().implementStateResponsibilities();
     }
 
     /**
      * If lie is true then the player who played before will pick up the deck
      */
     @Test
-    public void ifLiePreviousPlayerPicksUpDeck() {
+    public void ifLiePreviousPlayerPicksUpDeck() throws IllegalMoveException, IllegalCardException {
 
     }
 
@@ -25,7 +36,7 @@ public class CheatStateTester {
      * If lie is false then the player who called cheat picks up the deck
      */
     @Test
-    public void ifNotLieCheatPlayerPicksUpDeck() {
+    public void ifNotLieCheatPlayerPicksUpDeck() throws IllegalMoveException, IllegalCardException {
 
     }
 
@@ -34,7 +45,7 @@ public class CheatStateTester {
      * the gameState is changed to EndGameState
      */
     @Test
-    public void ifNotLieAndZeroCardsThenChangeGameState() {
-        
+    public void ifNotLieAndZeroCardsThenChangeGameState() throws IllegalMoveException, IllegalCardException {
+
     }
 }
